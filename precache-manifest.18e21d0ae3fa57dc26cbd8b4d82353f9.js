e687277aab4880933d3ee0611589ce9fde8b91cc
John Fitzgerald <j.a.fitzgerald@outlook.com>
2022-05-04 22:56:50 +0100

    Updates

---
+++ precache-manifest.18e21d0ae3fa57dc26cbd8b4d82353f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618ba1e3bfb69ad2b18a3a47e8fc1e19",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7c6564297fd79a3efc2e",
+    "url": "/home/static/css/main.e82fa1e2.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6564297fd79a3efc2e",
+    "url": "/home/static/js/main.3365737c.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7b51813cd883be7bcde8bc0b3fb5c537",
+    "url": "/home/static/media/2021_headshot_small.7b51813c.png"
+  },
+  {
+    "revision": "09649d4572cf30ebd9a05ca406c641fd",
+    "url": "/home/static/media/resume.09649d45.pdf"
+  }
+]);
